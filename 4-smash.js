84308fe564ac7fa4065103f3b2f7352ca7403db9
Kamlia Nur <kamlianur.kamli@gmail.com>
2024-05-22 23:51:57 +0700

    day #4

---
+++ 4-smash.js
@@ -0,0 +1,23 @@
+// WPU Coding Challange
+// 3/366
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/javascript
+
+// function smash (words) {
+//   return words.join(' ');
+// };
+
+function smash(words) {
+  let result = '';
+  for (let i = 0; i < words.length; i++){
+    result += words[i];
+    if(i != words.length - 1){
+      result += ' ';
+    }
+  }
+  return result;
+}
+
+// const smash = words => words.join(' ');
+
+console.info(smash(["hello", "world"]));
+console.info(smash(["this", "is", "a", "really", "long", "sentence"]));
