02fd9eeb51ba422727eb3f4f831909c88b4f2b07
Kamlia Nur <kamlianur.kamli@gmail.com>
2024-06-02 14:04:27 +0700

    day 10/366

---
+++ 10-stringToArray.js
@@ -0,0 +1,9 @@
+// WPU Coding Challange
+// 10/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+const stringToArray = (string) => string.split(' ');
+
+
+console.log(stringToArray("Robin Singh"));
+console.log(stringToArray("I love arrays they are my favorite"));
--- 9-maxmin.js
+++ 9-maxmin.js
@@ -1,5 +1,5 @@
 // WPU Coding Challange
-// 8/366
+// 9/366
 // https://www.codewars.com/kata/577a98a6ae28071780000989
 
 // var min = (list) => Math.min(...list);
